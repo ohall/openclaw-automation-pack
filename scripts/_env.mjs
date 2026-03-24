18bb0164f878f39c315bebe82c2dd9ae21576f42
Oakley Hall <oakley349@gmail.com>
2026-03-24 10:00:14 -0400

    refactor: add TODO for file existence check

--- scripts/_env.mjs
+++ scripts/_env.mjs
@@ -66,4 +66,4 @@ export function requireKeys(env, keys) {
  */
 export function httpToWs(url) {
   return url.replace(/^http:\/\//, 'ws://').replace(/^https:\/\//, 'wss://');
-}
+}// TODO: Add file existence check
